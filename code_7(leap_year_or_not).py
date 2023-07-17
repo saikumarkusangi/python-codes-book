74b364e5789e6e0b7f894ffe9c1db996feae8f4f
saikumarkusangi <saikumarkusangi007@gmail.com>
2023-07-17 16:11:18 +0530

    code book

---
+++ code_7(leap_year_or_not).py
@@ -0,0 +1,8 @@
+year = int(input('enter an year:'))
+
+if year % 400 ==0 and year % 100 == 0:
+        print(f'{year} is leap year')
+elif year % 4 == 0 and year % 100 != 0:
+        print(f'{year} is leap year')
+else:
+    print(f'{year} is not leap year')
